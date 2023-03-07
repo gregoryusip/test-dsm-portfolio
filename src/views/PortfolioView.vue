8c6cd94eb4dd97395789adb1461dab9001ed5474
Gregoryus Imannuel Perdana <gregoryus.imannuel200@gmail.com>
2023-03-07 22:57:17 +0700

    fix: use data json

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -28,58 +28,12 @@ import CardPortfolio from '../components/card/Portfolio.vue';
 </template>
 
 <script>
+import json from '@/json/data.json';
 export default {
   name: 'PortfolioPage',
   data() {
     return {
-      portfolios: [
-        {
-          id: '1',
-          img: ['portfolio-1.png'],
-          title:
-            'Decision Support Model Based on Fuzzy Logic To Determine Employee Salary',
-          author: 'Gregoryus Imannuel Perdana',
-          date: '26 December, 2022',
-          tag: 'Multistage Fuzzy Logic',
-          demoLink: 'https://greg-model-7sybag4rtq-et.a.run.app/',
-          dashboardLink: 'https://dashboard-salary.vercel.app/',
-        },
-        {
-          id: '2',
-          img: ['portfolio-2.png'],
-          title:
-            'Group Decision Support Model fo Tech-Based Startup Funding using Multistage Fuzzy Logic',
-          author: 'Muhammad Donny devanda',
-          date: '22 December, 2022',
-          tag: 'Multistage Fuzzy Logic',
-          demoLink: 'https://gdsm-web-7sybag4rtq-et.a.run.app/import_data',
-          dashboardLink: 'https://gdsm-web-7sybag4rtq-et.a.run.app/',
-        },
-        {
-          id: '3',
-          img: ['portfolio-3.png'],
-          title:
-            'Decision Support Model for Software Project Team Recruitment Based on Personality Type and Soft Skill',
-          author: 'Rizky Martin',
-          date: '25 December, 2022',
-          tag: 'Fuzzy Clustering',
-          demoLink: 'https://martin-model-7sybag4rtq-et.a.run.app/',
-          dashboardLink:
-            'https://app.powerbi.com/view?r=eyJrIjoiNDhhY2RhZDEtNDhiNi00NTY3LWI5M2ItZDRlMDkzMmU2YzczIiwidCI6IjM0ODViOTYzLTgyYmEtNGE2Zi04MTBmLWI1Y2MyMjZmZjg5OCIsImMiOjEwfQ%3D%3D&pageName=ReportSection9dc4d1529f22b55a3ba4',
-        },
-        {
-          id: '4',
-          img: ['portfolio-4.png'],
-          title:
-            'Decision Support Model based on Multi-Stage Fuzzy Logic for Determining Green Cities in Indonesia',
-          author: "Joshua La'la",
-          date: '25 December, 2022',
-          tag: 'Multistage Fuzzy Logic',
-          demoLink: 'https://joshua-model-7sybag4rtq-et.a.run.app/',
-          dashboardLink:
-            'https://app.powerbi.com/view?r=eyJrIjoiYzQxNWFkODAtNTA5Ni00MDViLWIyZjktNGI2NDQxMDliMDg3IiwidCI6IjM0ODViOTYzLTgyYmEtNGE2Zi04MTBmLWI1Y2MyMjZmZjg5OCIsImMiOjEwfQ%3D%3D',
-        },
-      ],
+      portfolios: json,
     };
   },
 };
