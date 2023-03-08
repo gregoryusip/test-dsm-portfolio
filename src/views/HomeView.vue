d5af7a05879437614a37580f4c052cee2dcc2707
Gregoryus Imannuel Perdana <gregoryus.imannuel200@gmail.com>
2023-03-08 11:13:45 +0700

    fix: update copywriting

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,9 +14,15 @@ import CardPortfolio from '../components/card/Portfolio.vue';
               Decision Support Model
             </h1>
             <p class="home--landing__container--subtitle">
-              DSM is a model that can provide problem-solving or decision
-              support used by organizations, individuals, companies, and others
-              to get maximum decisions.
+              A decision support model (DSM) is a type of computer-based model
+              that is designed and constructed to assist individuals or groups
+              in making complex decisions. It is a tool finally that helps
+              people analyze data, evaluate options, and make informed choices.
+              The goal of a DSM is to provide decision-makers with the
+              information and tools they need to make better decisions. This can
+              involve modelling data to identify patterns or trends, simulating
+              the effects of different scenarios, and providing the media to see
+              the suggested actionable decision.
             </p>
           </div>
           <div class="home--landing__container">
