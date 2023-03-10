a1930d4af9294e2b1ee6de04c0b18709c753b597
Gregoryus Imannuel Perdana <gregoryus.imannuel200@gmail.com>
2023-03-10 11:13:51 +0700

    feat: footer components

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="footer">
+    <div class="site-container">
+      <p class="footer-copyright">
+        © Copyright DSM Bina Nusantara University 2023. All rights reserved
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  data() {
+    return {};
+  },
+};
+</script>
